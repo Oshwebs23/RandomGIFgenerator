abfc4fa45cb4591d455413404183e88a136e9439
Oshwebs23 <osian1996@live.co.uk>
2025-05-27 21:01:46 +0100

    javascript complete, basic css styling

---
+++ script.js
@@ -0,0 +1,13 @@
+async function getRandomGif() {
+    const apiKey = 'Usx35ZNvkiFppl8mae3JyLLEkGGqE0pi';
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        document.getElementById('gif').src = data.data.images.original.url;
+    } catch (error) {
+        console.error('Error fetching GIF:', error);
+    }
+}
+
