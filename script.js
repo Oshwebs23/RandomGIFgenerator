47b5dd69fa6ec4891affab43c841e2f1f7bab41d
Oshwebs23 <osian1996@live.co.uk>
2025-06-07 18:13:26 +0100

    Added styling and h1 title

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getRandomGif() {
     const apiKey = 'Usx35ZNvkiFppl8mae3JyLLEkGGqE0pi';
     const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
 
-    try {
+    try {   
         const response = await fetch(url);
         const data = await response.json();
         document.getElementById('gif').src = data.data.images.original.url;
